0fcd73b2569d7b59470cf97cc5813178db654cd4
sergioduartte <sduartecc@gmail.com>
2019-03-01 15:02:06 -0300

    Mais coisinhas
    devagar se vai ao longe?

--- wtf/app.py
+++ wtf/app.py
@@ -7,16 +7,19 @@ merdao = 404
 
 @app.route("/")
 def hello_world():
-    return "iae essa galera!<strong> \n I'm learning Flask!!</strong>", ok
-    
-
+    return  "o/'", 200
 @app.route("/teste")
-def teste():
-    return "iae essa galera!<strong> \n Querendo...</strong>", ok
+@app.route("/teste/<algumaCoisa>")
+@app.route("/teste/<algumaCoisa>/<nome>")
+def imprime(algumaCoisa=None, nome=None):
+    if nome == "sergio":
+        return "Oi sejo! <br><strong> \n I'm learning Flask!!</strong>", ok
+    if algumaCoisa == "jooj":
+        return "iae essa galera! Teste.<br><strong>I'm learning Flask!!</strong>", ok
+    return "acho que deu certo", ok
 
+# @app.route("/teste/icaro")
+# def teste2():
+#     return "iae essa galera!<strong> \n oooooh Dessada Sama...</strong>", ok
 
-@app.route("/teste/icaro")
-def teste2():
-    return "iae essa galera!<strong> \n oooooh Dessada Sama...</strong>", ok
-
-app.run()
+app.run(debug=True, use_reloader=True) #mudar para false antes do deploy
